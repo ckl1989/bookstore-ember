c5b8ac7de5a2b7b4f924efec1178f3e6f7a440d6
Chris Lam <christopher.kong.lam@gmail.com>
2016-03-30 19:33:33 -0700

    introduce style sheet and create model hook for books route

--- app/models/author.js
+++ app/models/author.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 import Publisher from './publisher';
 
 export default Publisher.extend({
-books: DS.hasMany('book', { async: true })
+    books: DS.hasMany('book', { async: true })
 });
