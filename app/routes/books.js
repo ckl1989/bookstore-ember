a66b51f58b9b442993c65aaf249a86f616e8f7a6
Chris Lam <christopher.kong.lam@gmail.com>
2016-03-30 19:50:27 -0700

    set modal component to be rendered from template. blurred out background when modal is active

--- app/routes/books.js
+++ app/routes/books.js
@@ -3,5 +3,24 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('book');
+  },
+  actions: {
+    openCheckoutModal(book) {
+      this.controllerFor('application').set('showingModal', true);
+      return this.render('modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: book,
+        controller: 'application'
+      });
+    },
+
+    closeCheckoutModal() {
+      this.controllerFor('application').set('showingModal', true);
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
   }
 });
